acc2406a5870594cc17890d19e53a8ede4d875d6
Justice Selorm Bruec <JusticeSelormBruce@gmail.com>
2023-03-06 21:32:16 +0000

    Update session.js

--- session.js
+++ session.js
@@ -64,14 +64,14 @@ session.post = function (req, res, next) {
                     password: data,
                     tosAgreement: tosAgreement
                 }
-                db.send(user,collection_name,"insertOne",function (bool, promise) {
+                db.send(user, collection_name, "insertOne", function (bool, promise) {
                     if (bool && promise) {
                         console.log(promise)
                         res.status(200).send("user created successfully")
                     } else {
                         res.status(400).send('something happend')
                     }
-        
+
                 })
 
 
@@ -89,6 +89,124 @@ session.post = function (req, res, next) {
 
 
 }
+
+
+/**
+ * required field id
+ * @param req
+ * @param res
+ * 
+ */
+
+session.get = function (req, res, next) {
+    //validate field
+    var id = typeof req.query.id == 'string' && req.query.id.trim().length == 24 ? req.query.id.trim() : false;
+
+    if (id) {
+        // form filter
+        var searchObject = {
+            _id: new ObjectId(id)
+        }
+        //query db
+        db.send(searchObject, collection_name, "findOne", function (bool, promise) {
+            if (bool && promise) {
+                res.status(200).send(promise)
+            } else {
+                res.status(404).send('no match found')
+            }
+        })
+
+    } else {
+        res.status(401).send("search id does not meet required standard")
+    }
+}
+
+
+/**
+ * update
+ * required id
+ * optional  name,password
+ * @param req
+ * @param res
+ * @param next
+ */
+
+session.put = function (req, res, next) {
+    //validate field
+    var id = typeof req.query.id == 'string' && req.query.id.trim().length == 24 ? req.query.id.trim() : false;
+    var name = typeof req.body.name == 'string' && req.body.name.trim().length > 0 ? req.body.name.trim() : false;
+    var password = typeof req.body.password == 'string' && req.body.password.trim().length >= 8 ? req.body.password.trim() : false;
+    if (id && (name || password)) {
+        var data = {};
+        if (name) {
+            data.name = name
+
+        }
+        if (password) {
+            util.hashPassword(password, function (hashedPassword) {
+                if (hashedPassword) {
+                    data.password = hashedPassword
+                } else {
+                    res.status(500).send("internal server error, could not hash new password")
+                }
+            })
+
+        }
+        var queryObject = [
+            { _id: new ObjectId(id) },
+            { $set: data }
+        ]
+
+        db.send(queryObject, collection_name, "updateOne", function (bool, promise) {
+            if (bool && promise) {
+                res.status(200).send("record updated successfuly")
+
+            } else {
+                res.status(500).send("internal server error, we could not update your details, try again later")
+            }
+        })
+
+    } else {
+        res.status(403).send('all mandatory fields are required')
+    }
+
+}
+
+
+/**
+ * delete  required field = id
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+
+session.delete = function (req, res, next) {
+    var id = typeof req.query.id == 'string' && req.query.id.trim().length > 0 ? req.query.id.trim() : false;
+    if (id) {
+        var deleteObject = { _id: new ObjectId(id) }
+
+        db.send(deleteObject,collection_name, 'deleteOne', function (bool, promise) {
+            if (bool && promise.deletedCount == 1) {
+                console.log(promise)
+                res.status(200).send("record deleted successfully")
+            } else {
+                res.status(400).send('something happend')
+            }
+
+        })
+
+
+    } else {
+        res.status(401).send("valid post id is required to delete a post")
+    }
+
+
+}
+
+
+session.use(function (req, res) {
+    res.status(404).send("Sorry, can't find that!");
+});
 
 
 
